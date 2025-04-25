0378ee53ce2e6497a2cbf5795bbf058812d1bfac
Caleb Miller <calebmiller@uvic.ca>
2025-04-25 10:29:46 -0700

    added funcutionality to only do Q or only GCD frames or both

--- GeoSkimmer.py
+++ GeoSkimmer.py
@@ -8,8 +8,8 @@ icetray.I3Logger.global_logger.set_level(I3LogLevel.LOG_INFO)
 
 usage = "usage: %prog [options]"
 parser = ArgumentParser(usage)
-parser.add_argument("-i","--infile",default="input.i3",  help="read from infile (.i3{.gz} format)")
-parser.add_argument("-o","--outfile",default="output.i3",help="Write output to outfile (.i3{.gz} format)")
+parser.add_argument("-i","--infile",default=None,  help="read from infile (.i3{.gz} format)")
+parser.add_argument("-o","--outfile",default=None,help="Write output to outfile (.i3{.gz} format)")
 parser.add_argument("-s","--selectionfile",default="strings.csv",help="csv file with list of strings to keep in selection")
 parser.add_argument("-g","--gcdfile", default=None,help="read in gcdfile (.i3{.gz} format)")
 parser.add_argument("-t","--outgcd", default=None,help="filtered gcdfile (.i3{.gz} format)")
@@ -31,14 +31,19 @@ icetray.logging.log_info(f"selected strings: {allowed_strings}")
 
 tray = icetray.I3Tray()
 
-infiles=[infile]
+infiles = []
+if infile: infiles.append(infile)
 if ingcd: infiles.append(ingcd)
+
+if not infiles:
+    print("No input")
+    sys.exit(1) 
 
 tray.Add("I3Reader", FilenameList=infiles)
 
 tray.Add(FilterFrame, AllowedStrings=allowed_strings)
 
-tray.Add("I3Writer", Filename=outfile, Streams=[icetray.I3Frame.DAQ])
+if outfile: tray.Add("I3Writer", Filename=outfile, Streams=[icetray.I3Frame.DAQ])
 if outgcd: tray.Add("I3Writer", Filename=outgcd, Streams=[icetray.I3Frame.Geometry, icetray.I3Frame.Calibration, icetray.I3Frame.DetectorStatus])
 
 
